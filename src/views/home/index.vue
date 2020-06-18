cf0e707620f4591c94cba40de88cb6ecd219e4ef
wzg15801407309 <1051084301@qq.com>
2020-06-18 23:16:42 +0800

    格式设置

---
+++ src/views/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home" />
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() { },
+  mounted() { },
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+#home {
+}
+</style>
